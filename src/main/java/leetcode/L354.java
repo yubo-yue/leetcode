a135f4daa17d895ca13752d7827b8c7d94aa99d0
yubo <yubo.yue@gmail.com>
2025-04-22 10:17:58 +0800

    update

---
+++ src/main/java/leetcode/L354.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class L354 {
+    public int maxEnvelop(int[][] envelopes) {
+        int n = envelopes.length;
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+
+        int[] heights = new int[n];
+        for (int i = 0; i < n; i++) {
+            heights[i] = envelopes[i][1];
+        }
+        return lengthOfLIS(heights);
+    }
+
+    private int lengthOfLIS(int[] heights) {
+        int n = heights.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (heights[i] > heights[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
